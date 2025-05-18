6866f94731a7743449531d20ad6812bfe7db823a
Vikas <vikas.vikas.kushwaha@gmail.com>
2025-05-19 03:44:35 +0530

    secure :true

--- src/services/signupServices.js
+++ src/services/signupServices.js
@@ -47,7 +47,7 @@ const formatUserResponse = (user) => {
 const setTokenCookie = (res, token) => {
     res.cookie('token', token, {
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000
     });
